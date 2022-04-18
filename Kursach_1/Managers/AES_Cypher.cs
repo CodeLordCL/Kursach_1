ba81ac294b4e0326f6c3b864b179f2d2889267ee
Незилян Сергей <sega140900@mail.ru>
2022-04-18 22:58:45 +0300

    Init commit

---
+++ Kursach_1/Managers/AES_Cypher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Kursach_1.Managers
+{
+    internal class AES_Cypher
+    {
+        public string Encrypt_AES(string plain_text, string pass, byte[] vector)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(pass);
+
+            AesManaged aes_managed = new AesManaged();
+
+            aes_managed.Key = key;
+
+            aes_managed.IV = vector;
+
+            MemoryStream memory_stream = new MemoryStream();
+
+            CryptoStream crypto_stream = new CryptoStream(memory_stream, aes_managed.CreateEncryptor(), CryptoStreamMode.Write);
+
+            byte[] input = Encoding.ASCII.GetBytes(plain_text);
+
+            crypto_stream.Write(input, 0, input.Length);
+
+            crypto_stream.FlushFinalBlock();
+
+            byte[] encrypted = memory_stream.ToArray();
+
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public string Decrypt_AES(string plain_text, string pass, byte[] vector)
+        {
+            byte[] key = Encoding.ASCII.GetBytes(pass);
+
+            AesManaged aes_managed = new AesManaged();
+
+            aes_managed.Key = key;
+
+            aes_managed.IV = vector;
+
+            MemoryStream memory_stream = new MemoryStream();
+
+            CryptoStream crypto_stream = new CryptoStream(memory_stream, aes_managed.CreateDecryptor(), CryptoStreamMode.Write);
+
+            byte[] inputBytes = Convert.FromBase64String(plain_text);
+
+            crypto_stream.Write(inputBytes, 0, inputBytes.Length);
+
+            crypto_stream.FlushFinalBlock();
+
+            byte[] decrypted = memory_stream.ToArray();
+
+            return UTF8Encoding.ASCII.GetString(decrypted, 0, decrypted.Length);
+        }
+    }
+}
---
+++ Kursach_1/Managers/DES_Symmetric_Cypher.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Kursach_1.Managers
+{
+    internal class DESSymmetricCypher
+    {
+        string pass;
+        FileStream file_encr;
+        FileStream file_input;
+
+
+        public DESSymmetricCypher(string pass, string file_encr_name, string file_input_name) 
+        { 
+            this.pass = pass;
+            this.file_encr = new FileStream(file_encr_name, FileMode.Create, FileAccess.Write);
+            this.file_input = new FileStream(file_input_name, FileMode.Open, FileAccess.Read);
+        }
+        public void encrypt_des()
+        {
+
+            //FileStream file_encr = new FileStream("DES_sym_out1.txt", FileMode.Create, FileAccess.Write);
+
+            //FileStream file_input = new FileStream("DES_sym_in.txt", FileMode.Open, FileAccess.Read);
+
+            var des = new DESCryptoServiceProvider();
+            try
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(pass);
+
+                des.IV = ASCIIEncoding.ASCII.GetBytes(pass);
+
+                ICryptoTransform des_encr = des.CreateEncryptor();
+
+                CryptoStream stream = new CryptoStream(file_encr, des_encr, CryptoStreamMode.Write);
+
+                byte[] input_array = new byte[file_input.Length - 0];
+
+                file_input.Read(input_array, 0, input_array.Length);
+
+                stream.Write(input_array, 0, input_array.Length);
+
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return;
+        }
+
+        public void decrypt_des()
+        {
+            //FileStream file_encr = new FileStream("DES_sym_out2.txt", FileMode.Create, FileAccess.Write);
+
+            //FileStream file_input = new FileStream("DES_sym_out1.txt", FileMode.Open, FileAccess.Read);
+
+            var des = new DESCryptoServiceProvider();
+            try
+            {
+                des.Key = ASCIIEncoding.ASCII.GetBytes(pass);
+
+                des.IV = ASCIIEncoding.ASCII.GetBytes(pass);
+
+                ICryptoTransform des_encr = des.CreateDecryptor();
+
+                CryptoStream stream = new CryptoStream(file_encr, des_encr, CryptoStreamMode.Write);
+
+                byte[] bytearrayinput = new byte[file_input.Length - 0];
+
+                file_input.Read(bytearrayinput, 0, bytearrayinput.Length);
+
+                stream.Write(bytearrayinput, 0, bytearrayinput.Length);
+
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return;
+        }
+    }
+}
